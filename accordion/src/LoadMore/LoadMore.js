68b125da04a14048ef49509d43d49d292d5fa865
sanjitkumar28 <sanjit2806@gmail.com>
2025-01-17 23:37:32 +0530

    started load more

---
+++ accordion/src/LoadMore/LoadMore.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {useState, useEffect} from 'react'
+
+const LoadMore=()=>{
+
+    const [products,setProducts] = useState([]);
+    const [count,setCount] =useState(0);
+    const [loading,setLoading]= useState(false);
+      return(
+        <div>
+
+        </div>
+      )
+}
+export default LoadMore;
